4176d090b0d33f378c6b7df8165453dfa4a6e98c
neil-kuldip <neilkuldip2425@gmail.com>
2022-04-13 19:24:53 -0400

    refactor: converted components to functional via hooks

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 import AccountBalance from './AccountBalance';
 import { Link } from 'react-router-dom';
 
-class Credits extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Credits</h1>
-                <AccountBalance accountBalance={this.props.accountBalance} />
-                <Link to="/"><button>Home</button></Link>
-            </div>
-        );
-    }
-}
+const Credits = (props) => {
+    return (
+        <div className="App">
+            <h1>Credits</h1>
+            <AccountBalance accountBalance={props.accountBalance} />
+            <Link to="/"><button>Home</button></Link>
+        </div>
+    );
+};
 
 export default Credits;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css'
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 
-class Home extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Bank of React</h1>
-                <h4><em>What's in your JavaScript?</em></h4>
+const Home = (props) => {
+    return (
+        <div className="App">
+            <h1>Bank of React</h1>
+            <h4><em>What's in your JavaScript?</em></h4>
                 
-                <Link to="/userProfile"><button>User Profile</button></Link>
-                <Link to="/login"><button>Login</button></Link>
-                <Link to="/credits"><button>Credits</button></Link>
-                <br/><br/>
-                <AccountBalance accountBalance={this.props.accountBalance}/>
-            </div>
-        );
-    }
+            <Link to="/userProfile"><button>User Profile</button></Link>
+            <Link to="/login"><button>Login</button></Link>
+            <Link to="/credits"><button>Credits</button></Link>
+            <br/><br/>
+            <AccountBalance accountBalance={props.accountBalance}/>
+        </div>
+    );
 }
 
 export default Home;
